f47fcce47602e77d0d6d11ad16e3f0f4858cc241
Md Shahadat Hossain <mdridoysikder862@gmail.com>
2025-05-08 00:26:48 +0600

    Assignment 7  problems solved

--- assignment1.ts
+++ assignment1.ts
@@ -59,3 +59,37 @@ function processValue(value: string | number): number | undefined {
 }
 
 const processValueResult = processValue("1");
+
+interface Product {
+  name: string;
+  price: number;
+}
+
+function getMostExpensiveProduct(products: Product[]): Product | null {
+  if (products.length !== 0) {
+    const maxPrice = Math.max(...products.map((product) => product.price));
+    return products.find((product) => product.price === maxPrice)!;
+  } else {
+    return null;
+  }
+}
+
+const getMostExpensiveProductResult = getMostExpensiveProduct([]);
+
+enum Day {
+  Monday,
+  Tuesday,
+  Wednesday,
+  Thursday,
+  Friday,
+  Saturday,
+  Sunday,
+}
+
+function getDayType(day: Day): string {
+  if (day === Day.Saturday || day === Day.Sunday) {
+    return "Weekend";
+  } else {
+    return "Weekday";
+  }
+}
